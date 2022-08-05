8bcecded4521e8246715a2810f22c56f0f060a32
jake1025 <jake1025@gmail.com>
2022-08-04 22:44:43 -0400

    filter out non-pizza specials

--- poller.py
+++ poller.py
@@ -11,27 +11,47 @@ def request_html_text(url):
     return r.text
 
 
-def parse_specials_menu(html_text):
+def parse_pizza_specials(html_text, pizza_spelling='pizza', date_section_index=2):
     doc = html.fromstring(html_text.encode('unicode-escape'))
-    items = doc.find_class('menu-item')
-    contents = [item.getchildren()[0].text for item in items]
-    return contents
+    sections = doc.find_class('menu-section')
+    if not sections:
+        raise RuntimeError("Unable to parse specials menu.")
+
+    # Scrape menu date
+    menu_date = None
+    section_0_titles = sections[0].find_class('menu-item-title')
+    if section_0_titles and len(section_0_titles) == 3:
+        menu_date = section_0_titles[date_section_index].text
+
+    # Scrape pizza specials
+    pizza_specials = []
+    for s in sections[1:]:
+        menu_section_title_div = s.find_class('menu-section-header')[0].find_class('menu-section-title')[0]
+        special_category = menu_section_title_div.text
+        if special_category.strip().lower() == pizza_spelling.lower():
+            pizza_specials.extend([e.text for e in s.find_class('menu-item-title')])
+
+    cache = {'date': menu_date, 'pizzas': pizza_specials}
+    return cache
 
 
 def run(config):
+    url = config['specials-menu-url']
+    vodka_spelling = config['specials-menu-vodka-spelling']
+    pizza_spelling = config['specials-menu-pizza-spelling']
     config_date_index = config['specials-menu-date-index']
-    vodka_spelling = config['specials-menu-match-str']
-    html_text = request_html_text(config['specials-menu-url'])
-    menu_items = parse_specials_menu(html_text)
-    date_str = menu_items[config_date_index]
-    specials = menu_items[config_date_index + 1:]
+
+    html_text = request_html_text(url)
+    specials_cache = parse_pizza_specials(html_text, pizza_spelling, config_date_index)
+    date_str = specials_cache['date']
+    specials = specials_cache['pizzas']
     vodka_is_special = vodka_spelling.lower() in [c.strip().lower() for c in specials]
 
-    print(f"Lil' Frankie's specials on {date_str}:")
+    print(f"Lil' Frankie's pizza specials on {date_str}:")
     for i, item in enumerate(specials):
         print(f"{i + 1}. {item}")
     print("")
-    print(f"{vodka_spelling} pizza {'IS' if vodka_is_special else 'IS NOT'} on the specials menu for {date_str}")
+    print(f"{vodka_spelling} pizza *{'IS' if vodka_is_special else 'IS NOT'}* on the specials menu for {date_str}")
 
 
 def main():
